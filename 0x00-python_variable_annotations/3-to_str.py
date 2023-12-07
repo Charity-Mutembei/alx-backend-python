42000642cf76e8c01e3e83e0ad968874d38bdc77
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-07 13:48:05 +0300

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+function to_str with a float argument n
+returns a string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a floating-point number to its string representation.
+
+    Args:
+        n (float): The input floating-point number.
+
+    Returns:
+        str: The string representation of the input number.
+    """
+    return str(n)
