64a0a2e635aee4f00171cb189a7724e5e054e147
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-07 13:49:49 +0300

    task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+define and annotate the variables as
+specified
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
