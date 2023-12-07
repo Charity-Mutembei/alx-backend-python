2de47d7b12904898f5a91b25d8879fc372966231
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-07 13:39:22 +0300

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers.
+
+    Args:
+        a (float): The first operand.
+        b (float): The second operand.
+
+    Returns:
+        float: The sum of the two operands.
+    """
+    return a + b
