0a18f4e9bc32c4ce2ea52f2c9c34d90665ffddc1
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-07 14:10:47 +0300

    task 12

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -14,23 +14,3 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
         for i in range(factor)
     ]
     return zoomed_in
-
-
-array = [12, 72, 91]
-
-zoom_2x = zoom_array(array)
-
-"""
-The correct usage should be an integer as the factor
-"""
-
-
-zoom_3x = zoom_array(array, 3)
-
-"""
-You can also omit the type annotations
-in the call, mypy will infer them
-"""
-
-
-zoom_4x = zoom_array(array, 4)
