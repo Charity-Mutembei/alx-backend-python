42f6e8146650018ee3963402322d785d594862cd
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-07 13:43:12 +0300

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+function concat with str1 and str2 arguments
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
