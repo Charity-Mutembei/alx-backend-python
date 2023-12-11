55f9131c3a39450c456b757d020acba735fef982
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-11 12:33:15 +0300

    task 0

---
+++ 0x01-python_async_function/virtual_env_script.py
@@ -0,0 +1,7 @@
+# create a virtual environment
+python3 - m venv
+/home/cherry/Programming/ALX/alx-backend-python/0x01-python_async_function
+
+# Activate the virtual environment
+source / home/cherry/Programming/ALX/alx-backend - \
+    python/0x01-python_async_function/bin/activate
