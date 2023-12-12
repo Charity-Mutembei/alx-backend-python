6f4e7ca4808c549f21ec064c61dd4bcc7bb9f8c6
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-12 12:56:47 +0300

    task 1 - refactor #1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,11 +5,12 @@ and then write a coroutine called async_comprehension
 that takes no arguments.
 """
 import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """
     The coroutine will collect 10 random numbers using an
     async comprehensing over async_generator,
