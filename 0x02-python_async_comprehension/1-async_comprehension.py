8e1cc3a4df59c3b1b41f6cd1bc3220404ffa77cc
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-12 12:57:34 +0300

    task 1 - refactor #2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,7 +4,7 @@ Import async_generator from the previous task
 and then write a coroutine called async_comprehension
 that takes no arguments.
 """
-import asyncio
+
 from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
